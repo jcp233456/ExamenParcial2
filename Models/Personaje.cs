d5727ff8fd1f7c2a67b119fe55f030cfdc952eae
carlos peñalonzo <juancarlospenalonzochiguil1@gmail.com>
2026-05-21 08:32:06 -0600

    se crea personajes.razor para que pueda registrar los datos que se le solicita y que los guarde en la funcion para que luego los muestre

--- Models/Personaje.cs
+++ Models/Personaje.cs
@@ -4,7 +4,7 @@ namespace BlazorApp1.Models
 {
     public class Personaje
     {
-        public string personaje { get; set; }= string.Empty;
+        public string nombre { get; set; }= string.Empty;
         public int nivel { get; set; }
         public double ptsvida { get; set; }
         public int exp {  get; set; }
