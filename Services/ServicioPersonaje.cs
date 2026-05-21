4ec36f2a1074e845e9417a3407e7303bd3fd8db7
carlos peñalonzo <juancarlospenalonzochiguil1@gmail.com>
2026-05-21 08:22:59 -0600

    creacion de los servicios de  el personaje para que pueda guardar la informacion el json con los datos que se carguen en el ingreso

---
+++ Services/ServicioPersonaje.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp1.Models;
+using System.Text.Json;
+
+namespace BlazorApp1.Services
+{
+    public class ServicioPersonaje
+    {
+        private string ruta = "personajes.json";
+
+        public List<Personaje> personajes = new();
+
+        public void Guardar()
+        {
+            var json = JsonSerializer.Serialize(personajes);
+            File.WriteAllText(ruta, json);
+        }
+
+        public void Cargar()
+        {
+            if (File.Exists(ruta))
+            {
+                string json = File.ReadAllText(ruta);
+
+                personajes = JsonSerializer.Deserialize<List<Personaje>>(json)
+                             ?? new List<Personaje>();
+            }
+        }
+    }
+}
